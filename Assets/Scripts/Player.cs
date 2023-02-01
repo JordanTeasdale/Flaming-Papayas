bf957c7514c89f9fa79fba5085b5c48c7d776deb
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2023-02-01 11:43:41 -0500

    Added double jumping and side-to-side movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,34 +8,43 @@ public class Player : MonoBehaviour
     [SerializeField] uint numOfExtraJumps;
     uint currExtraJumps;
     bool hasJumped;
+    Vector2 movement;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         currExtraJumps = numOfExtraJumps;
         hasJumped = false;
+        movement = new Vector2(0, 0);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(currExtraJumps);
+        Debug.Log(Input.GetAxis("Horizontal"));
         Jump();
+        Movement();
+
     }
 
     void Jump() {
         if (Input.GetAxis("Jump") != 0) {
-            if (GetComponent<Rigidbody2D>().velocity.y <= 0 && GetComponent<Rigidbody2D>().velocity.y >= -0.005) {
-                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10f, 0);
+            if (rb.velocity.y <= 0 && GetComponent<Rigidbody2D>().velocity.y >= -0.005) {
+                rb.velocity = new Vector2(rb.velocity.x, 10f);
                 currExtraJumps = numOfExtraJumps;
             } else if (currExtraJumps > 0 && !hasJumped) {
-                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10f, 0);
+                rb.velocity = new Vector2(rb.velocity.x, 10f);
                     currExtraJumps--;
             }
             hasJumped = true;
         }
         if (Input.GetAxis("Jump") == 0)
             hasJumped = false;
+    }
 
+    void Movement() {
+        rb.velocity = new Vector2(5 * Input.GetAxis("Horizontal"), rb.velocity.y);
     }
 }
