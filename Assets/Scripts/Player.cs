b1aaa6227d02d1551ca45d17c143c5c1f0eac7d0
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2023-02-01 11:10:46 -0500

    Added ability to double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]GameObject feet;
+    [SerializeField] uint numOfExtraJumps;
+    uint currExtraJumps;
+    bool hasJumped;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currExtraJumps = numOfExtraJumps;
+        hasJumped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(GetComponent<Rigidbody2D>().velocity.y);
+        Debug.Log(currExtraJumps);
         Jump();
     }
 
     void Jump() {
-        if (GetComponent<Rigidbody2D>().velocity.y <= 0 && GetComponent<Rigidbody2D>().velocity.y >= -0.005) {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10f * Input.GetAxis("Jump"), 0);
+        if (Input.GetAxis("Jump") != 0) {
+            if (GetComponent<Rigidbody2D>().velocity.y <= 0 && GetComponent<Rigidbody2D>().velocity.y >= -0.005) {
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10f, 0);
+                currExtraJumps = numOfExtraJumps;
+            } else if (currExtraJumps > 0 && !hasJumped) {
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10f, 0);
+                    currExtraJumps--;
+            }
+            hasJumped = true;
         }
+        if (Input.GetAxis("Jump") == 0)
+            hasJumped = false;
+
     }
 }
