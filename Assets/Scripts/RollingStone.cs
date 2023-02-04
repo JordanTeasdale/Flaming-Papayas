96d50fe71cfe21f6c3abfb4af0a0e6ae2e8fdfdf
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2023-02-03 20:08:11 -0500

    Added Rolling Stone and got it moving back and forth

---
+++ Assets/Scripts/RollingStone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingStone : MonoBehaviour
+{
+    [SerializeField] float distance;
+    [SerializeField] float speed;
+    float distanceTravel = 0;
+    float movement = 0;
+    Vector3 currPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distanceTravel < distance) {
+            movement = speed * Time.deltaTime;
+            currPos = gameObject.transform.position;
+            gameObject.transform.position = new Vector3(currPos.x += movement, currPos.y, currPos.z);
+            distanceTravel += Mathf.Abs(movement);
+        }
+        else {
+            speed *= -1;
+            distanceTravel= 0;
+        }
+    }
+}
